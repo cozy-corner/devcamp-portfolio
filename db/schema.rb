6ba093b628a5a236fa8e3f162b141ee470df4778
cozy <sasakicozy@gmail.com>
2019-09-21 22:41:29 +0900

    Renamed project name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_123526) do
+ActiveRecord::Schema.define(version: 2019_09_21_130755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,12 @@ ActiveRecord::Schema.define(version: 2019_09_19_123526) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "my_portfolios", force: :cascade do |t|
+  create_table "portfolios", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
     t.text "body"
-    t.text "main_image"
-    t.text "thumb_image"
+    t.string "main_image"
+    t.string "thumb_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
