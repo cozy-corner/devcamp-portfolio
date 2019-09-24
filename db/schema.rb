290fb9f0ec1ec332e371b18cffae4906c5d887d2
cozy <sasakicozy@gmail.com>
2019-09-25 00:40:45 +0900

    Updated staus for enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_123755) do
+ActiveRecord::Schema.define(version: 2019_09_24_122125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_23_123755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
@@ -43,6 +44,8 @@ ActiveRecord::Schema.define(version: 2019_09_23_123755) do
     t.string "thumb_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_portfolios_on_slug", unique: true
   end
 
   create_table "skills", force: :cascade do |t|
