01134870768825dc210c0eece05258b702c4e5f5
cozy <sasakicozy@gmail.com>
2019-09-26 21:27:48 +0900

    Implemented Custom scope

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,12 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+
+  # Portfolio.angular
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  # Portfolio.ruby_on_rails_portfolio_items
+  scope :ruby_on_rails_portfolio_items  , -> {where(subtitle: 'Ruby on Rails')}
+
 end
