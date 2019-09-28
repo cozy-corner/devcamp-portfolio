8a76ea569ac56f788014cea19084f6f0012c15fc
cozy <sasakicozy@gmail.com>
2019-09-28 15:55:35 +0900

    Implemented concern for image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
   # Portfolio.angular
@@ -12,8 +13,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 
     # ||= means, if self.main_image = nil ~ end
   end
