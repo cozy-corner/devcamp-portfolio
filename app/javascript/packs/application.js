e971e76c08a2b900ffaae14f3b3ed4c545e5d8a7
cozy <sasakicozy@gmail.com>
2019-10-12 02:46:39 +0900

    Import boot strap javascripts by webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import 'bootstrap';
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -8,7 +12,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,6 +19,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+// require jquery
+// require popper
+// require bootstrap-sprockets
+// require_tree .
